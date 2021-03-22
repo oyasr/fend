265a944eacf6ad2b3e760eccd706e108f7228ff7
Osama Yasser <osamayasserr@gmail.com>
2021-03-22 22:11:12 +0200

    Setup client & tests

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,7 +1,17 @@
-import { checkForName } from './js/nameChecker'
+import { checkForURL } from './js/urlChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+// Sass style files
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 
-alert("I EXIST")
-console.log("CHANGE!!");
+// alert("I EXIST")
+// console.log("CHANGE!!");
+
+export {
+    checkForURL,
+    handleSubmit
+}
